408e206c37cd0800823e36a7a2a92cf0f447a927
Madelyn Romero <m.romero013@gmail.com>
2025-04-29 17:12:08 -0600

    Complete values and variables practice

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+// JavaScript Fundamentals 1
+
+
+// Values and Variables
+
+let country = "United States of America";
+let continent = "North America";
+let population = 340.1;
+
+console.log(country);
+console.log(continent);
+console.log(population);
