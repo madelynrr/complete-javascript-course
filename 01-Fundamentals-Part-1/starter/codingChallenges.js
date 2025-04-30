f38a2c864e15240e8bc6b46bc6b427d618367e05
Madelyn Romero <m.romero013@gmail.com>
2025-04-30 14:10:39 -0600

    Fix styling of coding challenge script

--- 01-Fundamentals-Part-1/starter/codingChallenges.js
+++ 01-Fundamentals-Part-1/starter/codingChallenges.js
@@ -1,30 +1,30 @@
 // JavaScript Fundamentals Part 1
 
-// Coding Challenge #1
-const weightMark = 78;
-const heightMark = 1.69;
+// // Coding Challenge #1
+// const weightMark = 78;
+// const heightMark = 1.69;
 
-const weightJohn = 92;
-const heightJohn = 1.95;
+// const weightJohn = 92;
+// const heightJohn = 1.95;
 
-// const weightMark = 95;
-// const heightMark = 1.88;
+// // const weightMark = 95;
+// // const heightMark = 1.88;
 
-// const weightJohn = 85;
-// const heightJohn = 1.76;
+// // const weightJohn = 85;
+// // const heightJohn = 1.76;
 
-const bmiMark = weightMark / heightMark ** 2;
-const bmiJohn = weightJohn / (heightJohn * heightJohn);
+// const bmiMark = weightMark / heightMark ** 2;
+// const bmiJohn = weightJohn / (heightJohn * heightJohn);
 
-const markHigherBMI = bmiMark > bmiJohn;
+// const markHigherBMI = bmiMark > bmiJohn;
 
 // console.log(bmiMark, bmiJohn, markHigherBMI);
 
 
-// Coding Challenge #2
 
-if (bmiMark > bmiJohn) {
-    console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`);
-} else {
-    console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`);
-}
+// // Coding Challenge #2
+// if (bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`);
+// } else {
+//     console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`);
+// }
