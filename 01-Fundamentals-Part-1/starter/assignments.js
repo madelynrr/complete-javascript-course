301f2ddf03c0368ff713faccc266487d852c3fe2
Madelyn Romero <m.romero013@gmail.com>
2025-04-30 15:23:36 -0600

    Complete equality operators practice

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -71,3 +71,16 @@
 // console.log("19" - "13" + 17); // 23
 // console.log("123" < 57); // false
 // console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
+
+
+
+// // Equality Operators: == vs ===
+// const numNeighbors = Number(prompt("How many neighboring countries does your country have?"));
+
+// if (numNeighbors === 1) {
+//     console.log("Only 1 border!");
+// } else if (numNeighbors > 1) {
+//     console.log("More than 1 border");
+// } else {
+//     console.log("No borders");
+// }
