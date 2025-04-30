cd44c0435b7c7705d5cc02169963bddf6b5eccd8
Madelyn Romero <m.romero013@gmail.com>
2025-04-30 14:00:17 -0600

    Adding notes from lessons so far

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,86 @@
+// // Basic Operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
+
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "Madelyn";
+// const lastName = "Romero";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+
+// console.log(x);
+
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+
+// // Operator Precendence
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+
+// x = y = 25 - 10 - 5;
+
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge);
+
+
+// // Strings and Template Literals
+// const firstName = "Madelyn";
+// const job = "Software Engineer";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const madelyn = `I'm ` + firstName + " a " + (year - birthYear) + " year old " + job + "!";
+// const madelynNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+
+// console.log(madelyn);
+// console.log(madelynNew);
+
+// console.log("String with \n\
+//     multiple \n\
+//     lines");
+
+// console.log(`String with
+//     multiple
+//     lines`);
+
+
+// // Type Conversion and Coercion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Madelyn"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+
+// console.log(`I am ` + 23 + ` years old`);
+// console.log("23" - "10" - 3);
+
+// console.log(typeof ("1" + 1));
+// console.log(typeof (1 + "1"));
