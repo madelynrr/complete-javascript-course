173fa051b40307237a5926f393be7123e3b6420d
Madelyn Romero <m.romero013@gmail.com>
2025-05-02 14:44:49 -0600

    Complete Arrow Functions practice

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -16,29 +16,43 @@
 
 
 
-// Function Declarations vs Expressions
-function percentageOfWorld1(population) {
-    return (population / 7900) * 100;
-}
-const percentageChina = percentageOfWorld1(1441);
-console.log(percentageChina);
+// // Function Declarations vs Expressions
+// function percentageOfWorld1(population) {
+//     return (population / 7900) * 100;
+// }
+// const percentageChina = percentageOfWorld1(1441);
+// console.log(percentageChina);
 
-const percentageUnitedStates = percentageOfWorld1(340.1);
-console.log(percentageUnitedStates);
+// const percentageUnitedStates = percentageOfWorld1(340.1);
+// console.log(percentageUnitedStates);
 
-const percentageIceland = percentageOfWorld1(.39);
-console.log(percentageIceland);
+// const percentageIceland = percentageOfWorld1(.39);
+// console.log(percentageIceland);
 
 
-const percentageOfWorld2 = function (population) {
-    return (population / 7900) * 100;
-}
+// const percentageOfWorld2 = function (population) {
+//     return (population / 7900) * 100;
+// }
 
-const percentageChina2 = percentageOfWorld2(1441);
-console.log(percentageChina2);
+// const percentageChina2 = percentageOfWorld2(1441);
+// console.log(percentageChina2);
 
-const percentageUnitedStates2 = percentageOfWorld2(340.1);
-console.log(percentageUnitedStates2);
+// const percentageUnitedStates2 = percentageOfWorld2(340.1);
+// console.log(percentageUnitedStates2);
 
-const percentageIceland2 = percentageOfWorld2(.39);
-console.log(percentageIceland2);
+// const percentageIceland2 = percentageOfWorld2(.39);
+// console.log(percentageIceland2);
+
+
+
+// Arrow Functions
+const percentageOfWorld3 = population => (population / 7900) * 100;
+
+const percentageChina3 = percentageOfWorld3(1441);
+console.log(percentageChina3);
+
+const percentageUnitedStates3 = percentageOfWorld3(340.1);
+console.log(percentageUnitedStates3);
+
+const percentageIceland3 = percentageOfWorld3(.39);
+console.log(percentageIceland3);
