8fdf53da398fcdeacc99a8e62d096774ccf3698d
Madelyn Romero <m.romero013@gmail.com>
2025-05-02 15:15:57 -0600

    Add Reviewing Functions notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -105,3 +105,27 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 */
+
+
+
+/*
+// Reviewing Functions
+const calcAge = function (birthYear) {
+    return 2037 - birthYear
+}
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1991, "Madelyn"));
+console.log(yearsUntilRetirement(1970, "Bob"));
+*/
