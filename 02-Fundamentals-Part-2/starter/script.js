f7687eac8a5f4d53f6f803aff33c674275e563f5
Madelyn Romero <m.romero013@gmail.com>
2025-05-02 13:34:51 -0600

    Add Functions notes

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+
+if (hasDriversLicense) {
+    console.log("I can drive");
+}
+
+// const interface = "Audio";
+// const private = 2;
+// const if = 23;
+*/
+
+
+
+/*
+// Functions
+function logger() {
+    console.log("My name is Madelyn");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
