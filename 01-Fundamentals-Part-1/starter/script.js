56c29560aae9e12a380d6a106a9ca57fa1f030b9
Madelyn Romero <m.romero013@gmail.com>
2025-05-01 18:34:51 -0600

    Add Conditional/Ternary Operator notes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -281,3 +281,26 @@ console.log(`I'm ${2037 - 1991} years old`);
 //     const str = "23 is bigger";
 // }}`);
 */
+
+
+
+/*
+// The Conditional (Ternary) Operator
+const age = 23;
+
+age >= 18 ? console.log("I like to drink wine") : console.log("Cannot drink yet");
+
+const drink = age >= 18 ? "wine" : "water";
+
+console.log(`${drink}`);
+
+let drink2;
+if (age >= 18) {
+    drink2 = "wine"
+} else {
+    drink2 = "water"
+}
+console.log(`${drink2}`);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
