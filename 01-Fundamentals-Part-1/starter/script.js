24135e90f9e123b07a6c625235bee7f08b6c5c91
Madelyn Romero <m.romero013@gmail.com>
2025-05-01 18:25:46 -0600

    Add Statements and Expressions notes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -262,3 +262,22 @@ if (day === "monday") {
     console.log("Not a valid day");
 }
 */
+
+
+
+/*
+// Statements and Expressions
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+    const str = "23 is bigger";
+}
+
+console.log(`I'm ${2037 - 1991} years old`);
+
+// console.log(`${if (23 > 10) {
+//     const str = "23 is bigger";
+// }}`);
+*/
