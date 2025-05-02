47459d4cb06501afccd3339a958c0cce634db2c7
Madelyn Romero <m.romero013@gmail.com>
2025-05-02 14:41:01 -0600

    Add Arrow Function notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -61,3 +61,28 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991)
 console.log(age2);
 */
+
+
+
+/*
+// Arrow Functions
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age2);
+
+// Arrow Function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, "Madelyn"));
+*/
