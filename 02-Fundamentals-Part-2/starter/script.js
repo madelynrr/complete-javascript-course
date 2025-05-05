12eb1c382b19b6fa1609a4283228e58dcd9ae577
Madelyn Romero <m.romero013@gmail.com>
2025-05-05 14:38:00 -0600

    Add Introduction to Arrays notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -129,3 +129,45 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1991, "Madelyn"));
 console.log(yearsUntilRetirement(1970, "Bob"));
 */
+
+
+
+/*
+// Introduction to Arrays
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+// friends = ["Bob", "Alice"]
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const jonas = ["Madelyn", "Romero", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const birthYears = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(birthYears[0]);
+const age2 = calcAge(birthYears[1]);
+const age3 = calcAge(birthYears[birthYears.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(birthYears[0]), calcAge(birthYears[1]), calcAge(birthYears[birthYears.length - 1])];
+console.log(ages);
+*/
