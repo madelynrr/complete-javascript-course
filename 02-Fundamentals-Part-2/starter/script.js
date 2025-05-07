46b834b09728f642ae7e23a3238779623d742029
Madelyn Romero <m.romero013@gmail.com>
2025-05-06 19:28:06 -0600

    Add The While Loop notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -371,3 +371,29 @@ for (let exercise = 1; exercise <= 3; exercise++) {
     }
 }
 */
+
+
+
+/*
+// The While Loop
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) {
+        console.log(`Loop is about to end ${dice}`);
+    }
+}
+*/
