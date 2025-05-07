5b0fd687b0be353f14575a937abde544cd029c97
Madelyn Romero <m.romero013@gmail.com>
2025-05-07 15:38:17 -0600

    Complete Coding Challenge #1

---
+++ 03-Developer-Skills/starter/codingChallenges.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// // Iterate over array, log temperature and count
+// const temps1 = [17, 21, 23];
+// const temps2 = [12, 5, -5, 0, 4];
+
+// const printForecast = function (arr) {
+//   let string = "... ";
+//   for (let i = 0; i < arr.length; i++) {
+//     string += `${arr[i]}C in ${i + 1} days ... `;
+//   }
+//   console.log(string);
+// };
+
+// printForecast(temps2);
