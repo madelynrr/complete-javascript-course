4fab3a5f48dda1ed8f149169379727f299dd13ee
Madelyn Romero <m.romero013@gmail.com>
2025-05-06 18:10:54 -0600

    Add Iteration: The For Loop notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -304,3 +304,12 @@ console.log(madelyn.age);
 
 console.log(madelyn.getSummary());
 */
+
+
+
+/*
+// Iteration: The For Loop
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights: repetition ${rep}`);
+}
+*/
