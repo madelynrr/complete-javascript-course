40322d246448f3a46c0c3f50780bc426f38f90e8
Madelyn Romero <m.romero013@gmail.com>
2025-05-06 18:30:22 -0600

    Add Looping Arrays, Breaking, and Continuing notes

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -313,3 +313,42 @@ for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights: repetition ${rep}`);
 }
 */
+
+
+
+/*
+// Looping Arrays, Breaking, and Continuing
+const jonasArray = ["Madelyn", "Romero", 2037 - 1991, "teacher", ["Michael", "Peter", "Steven"], true];
+const types = [];
+const types2 = [];
+
+for (let i = 0; i < jonasArray.length; i++) {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+    types[i] = typeof jonasArray[i];
+    types2.push(typeof jonasArray[i]);
+}
+
+console.log(types);
+console.log(types2);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] !== "string") continue;
+
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] === "number") break;
+
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
+*/
