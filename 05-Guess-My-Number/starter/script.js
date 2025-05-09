ace5c2072744af1312fed671a75cccd92b6b2c51
Madelyn Romero <m.romero013@gmail.com>
2025-05-09 15:20:54 -0600

    Implement addEventListener for check button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,3 +11,44 @@ document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // When no number
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number!";
+
+    // When wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // When too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    // When too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
