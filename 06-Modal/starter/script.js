e1194339301097b3a2ed836ea834532805fcbca1
Madelyn Romero <m.romero013@gmail.com>
2025-05-09 19:00:54 -0600

    Implement close modal functionality for button and overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,9 +6,18 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", function () {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
   });
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
