30843617d664c26438bd97a723c49ffd147bda36
Madelyn Romero <m.romero013@gmail.com>
2025-05-09 18:51:54 -0600

    Add modal and overlay display logic

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,5 +7,8 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 }
