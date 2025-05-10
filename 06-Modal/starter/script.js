88ab8e7f501711baa4ba03eebcf40be1b50d605a
Madelyn Romero <m.romero013@gmail.com>
2025-05-09 19:28:48 -0600

    Implement close modal on esc keydown

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,11 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 const closeModal = function () {
   modal.classList.add("hidden");
@@ -12,12 +16,14 @@ const closeModal = function () {
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
 
 btnCloseModal.addEventListener("click", closeModal);
-
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
