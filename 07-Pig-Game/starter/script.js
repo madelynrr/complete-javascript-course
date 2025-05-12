81deba3a39652ec21369303306ae834e75ecdf9b
Madelyn Romero <m.romero013@gmail.com>
2025-05-12 17:52:34 -0600

    Select elements from HTML

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+// Selecting Elements
+const player0Element = document.querySelector(".player--0");
+const player1Element = document.querySelector(".player--1");
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+const current0Element = document.getElementById("current--0");
+const current1Element = document.getElementById("current--1");
+
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
