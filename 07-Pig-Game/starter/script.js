b3df303708705ff9c848282873eb6277d5bbf2c4
Madelyn Romero <m.romero013@gmail.com>
2025-05-12 17:54:04 -0600

    Add event listener for roll dice and switch player function

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -39,3 +39,30 @@ const init = function () {
 };
 
 init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle("player--active");
+  player1Element.classList.toggle("player--active");
+};
+
+// Roll Dice
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceElement.classList.remove("hidden");
+    diceElement.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to other player
+      switchPlayer();
+    }
+  }
+});
